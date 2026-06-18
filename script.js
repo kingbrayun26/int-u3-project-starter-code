f8aab7933f96ed1c1377f764e204b2edcd71b5a1
kingbrayun26 <brayun.kk@gmail.com>
2026-06-18 10:20:35 -0500

    Game Ver 1.0.1

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Declare variables below to save the different divs of your story.
+listScreen = document.querySelector('.list-screen');
+optionOneScreen = document.querySelector('.option-one-screen');
+optionTwoScreen = document.querySelector('.option-two-screen');
+optionOneEnd = document.querySelector('.option-one-end');
+optionTwoEnd = document.querySelector('.option-two-end');
  
 
 
